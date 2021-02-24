67bddc930f02771d167586319badd938e89b0ea2
Roni Laukkarinen <roni@dude.fi>
2021-02-24 08:03:41 +0200

    Make colors support 256 systems

--- motd.sh
+++ motd.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 bold=$(tput bold)
 normal=$(tput sgr0)
+PULINAGREEN_RGB='\033[38;2;32;255;208m' # Not supported by most clients
+PULINAGREEN=$(tput setaf 49)
+HOTPINK_RGB='\033[38;2;255;64;132m'
+HOTPINK=$(tput setaf 198)
+VIOLET_RGB='\033[38;2;136;108;228m'
+VIOLET=$(tput setaf 99)
+WHITE='\e[0;37m'
 NO_COLOR='\e[0m'
 export LC_ALL=fi_FI.UTF-8
 ecport LANG=fi_FI.UTF-8
 user="$(grep 'sshd' /var/log/auth.log | awk '/session opened for user/{print $11}' | tail -1)"
-echo "\033[38;2;32;255;208m"
+echo "${PULINAGREEN}"
 cat /home/rolle/shell/pulinabanner
-echo "\033[38;2;255;255;255m"
+echo "${WHITE}"
 echo "Moi ${bold}$user${normal},"
 echo ""
 echo "Tänään on $(date +'%A') ja kello on $(date +'%H.%M'). Sää juuri nyt:"
 echo ""
 cat /home/rolle/shell/weather
 echo ""
-echo "Käytäthän komentoa \033[38;2;255;64;132mtmux a\033[38;2;255;255;255m screenin sijaan. Kiitos."
+echo "Käytäthän komentoa ${HOTPINK}tmux a${WHITE} screenin sijaan. Kiitos."
 echo ""
-echo "${bold}Ohjeet${normal}: \033[38;2;32;255;208mhttps://www.pulina.fi/ohjeet/pulina-irc-shell-kayttoohje/\033[38;2;255;255;255m"
+echo "${bold}Ohjeet${normal}: ${PULINAGREEN}https://www.pulina.fi/ohjeet/pulina-irc-shell-kayttoohje/${WHITE}"
 echo ""
 echo "Hauskaa irkkailua!"
 echo ""
-echo "👾 \033[38;2;136;108;228m${bold}rolle${normal}${NO_COLOR}"
+echo "👾 ${bold}${VIOLET}rolle${normal}${NO_COLOR}"
 echo ""
 unset LC_ALL
 unset LANG
